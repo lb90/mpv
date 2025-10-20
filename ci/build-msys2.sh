531f263cf30e9465879b49dd14e9bb79f01db9fa
Luca Bacci <luca.bacci982@gmail.com>
2025-10-20 15:34:32 +0200

    Check CLANGARM64

--- ci/build-msys2.sh
+++ ci/build-msys2.sh
@@ -2,16 +2,10 @@
 
 . ./ci/build-common.sh
 
-args=(
-  -D{cdda,d3d-hwaccel,d3d11,dvdnav,jpeg,lcms2,libarchive}=enabled
-  -D{libbluray,lua,shaderc,spirv-cross,uchardet,vapoursynth}=enabled
-  -D{egl-angle-lib,egl-angle-win32,pdf-build,rubberband,win32-smtc}=enabled
-)
+pwd
+ls
 
-[[ "$SYS" == "clang64" ]] && args+=(
-  -Db_sanitize=address,undefined
-)
+#lldb --batch -o 'b main' -o 'run' -o 'b ExitProcess' -o 'b TerminateProcess' -o 'continue' -o 'bt' -o 'quit' -k 'bt' -k 'quit' -- rst2pdf.exe -c -b 1 --repeat-table-rows DOCS/man/mpv.rst -o mpv.pdf | cat
 
-meson setup build $common_args "${args[@]}"
-meson compile -C build
-./build/mpv.com -v --no-config
+
+lldb --batch -o 'run' -o 'quit' -k 'bt' -k 'image list' -k 'quit' -- python /c/a/_temp/msys64/clangarm64/bin/rst2pdf.exe -c -b 1 --repeat-table-rows DOCS/man/mpv.rst -o mpv.pdf | cat
